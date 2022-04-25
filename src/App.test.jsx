58a7d0587a425b2d0104b1aa5d70904e5a1d004c
aidanliddiard <aidanliddiard@gmail.com>
2022-04-25 16:21:32 -0700

    refactoring header test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,11 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
   it('Should render the header', async () => {
     render(<App />)
-    await screen.getByAltText('Alchemy Logo')
+    screen.getByAltText('Alchemy Logo')
 
-    return waitFor(() => {
-      screen.getByText('Vonta')
-    })
+    await screen.findByText('Vonta')
   })
 })
